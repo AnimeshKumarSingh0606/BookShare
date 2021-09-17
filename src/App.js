04a671de6e684f738b7f05961796f681fca976a0
Animesh Kumar Singh <animesh.2001singh@gmail.com>
2021-09-17 13:27:55 +0530

    Eveything getting ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBooks from './components/SearchBooks.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <SearchBooks/>
     </div>
   );
 }
---
+++ src/components/dummydata.js
@@ -0,0 +1,76 @@
+const collegename=[
+    {
+        value:"CET",
+        cname:"CET Bhubaneswar",
+    },
+    {
+        value:"KIIT",
+        cname:"KIIT Bhubaneswar",
+    },
+    {
+        value:"SOA",
+        cname:"SOA Bhubaneswar",
+    },
+    {
+        value:"GIET",
+        cname:"GIET Bhubaneswar",
+    }
+];
+
+const subjectname=[
+    {
+        value:"Mathematics",
+        sname:"Mathematics",
+    },
+    {
+        value:"Operating System",
+        sname:"Operating System",
+    },
+    {
+        value:"Computer Networks",
+        sname:"Computer Networks",
+    },
+    {
+        value:"DBMS",
+        sname:"DBMS",
+    }
+];
+
+const semester=[
+
+    {
+        value:1,
+        sem:"1st",
+    },
+    {
+        value:2,
+        sem:"2nd",
+    },
+    {
+        value:3,
+        sem:"3rd",
+    },
+    {
+        value:4,
+        sem:"4th",
+    },
+    {
+        value:5,
+        sem:"5th",
+    },
+    {
+        value:6,
+        sem:"6th",
+    },
+    {
+        value:7,
+        sem:"7th",
+    },
+    {
+        value:8,
+        sem:"8th",
+    },
+
+]
+
+export {collegename,subjectname,semester};
